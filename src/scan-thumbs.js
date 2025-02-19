36a13640872676b36836b0c0bc1913fe33b612fa
Krzysztof Kostałkowicz <12763939+krzykos@users.noreply.github.com>
2025-02-19 23:39:21 +0100

    Add compilations screenshots

--- src/scan-thumbs.js
+++ src/scan-thumbs.js
@@ -14,12 +14,13 @@ let totalGames = 0;
 let updatedThumbs = 0;
 Object.entries(screenshots).forEach(([platform, games]) => {
   Object.entries(games).forEach(([game, index]) => {
-    const screenshotsDef = JSON.parse(fs.readFileSync(path.join(dir, 'screenshots', index)));
+    const screenshotsPath = path.join(dir, 'screenshots', index);
+    const screenshotsDef = JSON.parse(fs.readFileSync(screenshotsPath));
     const title = path.join(dir, 'screenshots', index, '..', screenshotsDef[0].screenshots[0]);
     const list = path.join(dir, 'thumbs', index, '..', 'list.webp');
     totalGames++;
     fs.mkdirSync(path.dirname(list), { recursive: true });
-    if (!fs.existsSync(list) || fs.statSync(list).mtime < fs.statSync(path.join(dir, 'screenshots', index)).mtime) {
+    if (!fs.existsSync(list) || fs.statSync(list).mtime < fs.statSync(screenshotsPath).mtime) {
       console.log(' * ' + game + ' [list]');
       updatedThumbs++;
       resize(title, '80x', list);
@@ -28,6 +29,22 @@ Object.entries(screenshots).forEach(([platform, games]) => {
 });
 console.log(`total games: ${totalGames}`);
 console.log(`updated thumbs: ${updatedThumbs}`);
+
+let updatedCompilations = 0;
+const compilationsIndexPath = path.join(dir, 'screenshots', 'compilations', 'index.json');
+const compilationsIndex = JSON.parse(fs.readFileSync(compilationsIndexPath));
+compilationsIndex.index.forEach(({ url }) => {
+  const compilation = path.join(dir, 'screenshots', 'compilations', url);
+  const thumb = path.join(dir, 'thumbs', 'compilations', url);
+  if (!fs.existsSync(thumb) || fs.statSync(thumb).mtime < fs.statSync(compilationsIndexPath).mtime) {
+    console.log(' * ' + url + ' [compilation]');
+    updatedCompilations++;
+    resize(compilation, '160x', thumb);
+  }
+});
+const compilationsOutPath = path.join(dir, 'thumbs', 'compilations', 'index.json');
+fs.writeFileSync(compilationsOutPath, JSON.stringify(compilationsIndex, null, 2));
+console.log(`updated compilations: ${updatedCompilations}`);
 
 function resize(inFile, sizeSpec, outFile) {
   child_process.execFileSync('node', [RESIZE_JS, sizeSpec, inFile, outFile]);
